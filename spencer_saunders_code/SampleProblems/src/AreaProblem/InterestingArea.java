208a8087a5ceed22b99fcba52d531ddb5e2d06f8
Spencer Saunders <svs1@math.sc.edu>
2018-02-06 15:26:07 -0500

    Sample Problems from class

--- spencer_saunders_code/SampleProblems/src/AreaProblem/InterestingArea.java
+++ spencer_saunders_code/SampleProblems/src/AreaProblem/InterestingArea.java
@@ -4,8 +4,8 @@ public class InterestingArea {
     public static void main(String[] args){
         int area_1 = 1;
         //odd square with 4 extras
-        for(int n=2; n<=20; n++){
-            int area = (2*n-3)*(2*n-3) + 4; //only for n>=2
+        for(int n=3; n<=20; n++){
+            int area = 2*n*n-2*n+1; //only for n>=2
             System.out.println("Area of shape is: " + area);
         }
     }
