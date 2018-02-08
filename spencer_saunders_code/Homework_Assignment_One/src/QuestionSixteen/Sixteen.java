c90ccc45d360e826876cb38f7465e77aeb5e9ae5
Spencer Saunders <svs1@math.sc.edu>
2018-02-08 13:34:33 -0500

    revised Homework_Assignment_One

--- spencer_saunders_code/Homework_Assignment_One/src/QuestionSixteen/Sixteen.java
+++ spencer_saunders_code/Homework_Assignment_One/src/QuestionSixteen/Sixteen.java
@@ -1,16 +1,10 @@
 package QuestionSixteen;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Sixteen {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        System.out.println("Please enter any message:");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String message = reader.readLine();
-        System.out.println(message.length());
+        for (String input : args) {
+            System.out.println(input.length());
+        }
     }
 }
