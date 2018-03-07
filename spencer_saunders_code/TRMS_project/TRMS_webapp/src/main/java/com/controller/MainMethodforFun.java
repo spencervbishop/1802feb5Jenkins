6454a24da48f21caae90f3b181e5be95a7a66174
Spencer Saunders <svs1@math.sc.edu>
2018-03-07 18:35:38 -0500

    merging front end and back end into one folder

---
+++ spencer_saunders_code/TRMS_project/TRMS_webapp/src/main/java/com/controller/MainMethodforFun.java
@@ -0,0 +1,27 @@
+package com.controller;
+
+import com.beans.Employee;
+import com.beans.Supervisor;
+import com.dao.EmployeeJDBC;
+import com.services.EmployeeService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainMethodforFun {
+    public static void main(String[] args) throws Exception{
+//        EmployeeService employeeService = new EmployeeService();
+//        //Employee emp = new Employee("spencer", "saunders", "spencer111", "tylerbishop", 1, true, false, false, 1, 1000.00);
+//
+//        Employee emp = employeeService.getEmployee("depthead1");
+//        System.out.println(emp.getPassword());
+        EmployeeService service = new EmployeeService();
+        EmployeeController controller = new EmployeeController();
+        String username = "depthead1";
+        String password = "depthead1";
+
+        System.out.println(service.getEmployee(username));
+
+        System.out.println(controller.getEmployee(username, password).toString());
+    }
+}
